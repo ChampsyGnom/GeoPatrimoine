4cbeaa64ab49696b90dd2c3ad1beaba2ffc28ddf
ChampsyGnom <champsy.dev.ecarto@gmail.com>
2013-12-14 20:38:35 +0100

    Ajout de la librairies d'OpenLayers 3

--- data/rest-database-source.php
+++ data/rest-database-source.php
@@ -8,14 +8,24 @@ class RestDatabaseSource extends RestAbstractSource
     public function __construct($api)
     {
         parent::__construct($api);
+        $ini_array = parse_ini_file("./database.ini", $process_sections = true);
     }
     
      
      function Select() 
      {
-        return "oki";     
+        $isLogin = false;
+		    if (isset($_GET['isLogin']))
+        {$isLogin = filter_var($_GET['isLogin'], FILTER_VALIDATE_BOOLEAN);}
+        if ($isLogin)
+        {return $this->Login();}
+        return "non implémenté";     
      }
      
+     function Login()
+     {
+        return  "oki";
+     }
  }
 
 ?>
